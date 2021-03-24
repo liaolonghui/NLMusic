73a233f0c0cef8bb5ce8e8d372914df3fab4018c
liaolonghui <1340881930@qq.com>
2021-03-24 22:45:36 +0800

    美化

--- qq-admin/src/views/Main.vue
+++ qq-admin/src/views/Main.vue
@@ -4,7 +4,10 @@
       <div>
         <img src="../assets/logo.jpg" alt="logo" style="width: 40px; height:40px;vertical-align: -15px; margin: 5px;" />
         <span>南理音乐后台管理系统</span>
-        <span class="current">当前用户：{{name}}</span>
+        <span class="current">
+          <el-tag v-if="type == 'root'" type="danger">超级管理员：{{name}}</el-tag>
+          <el-tag v-else type="success">普通只读管理员：{{name}}</el-tag>
+        </span>
         <span class="out" @click="logout">退出登录</span>
       </div>
     </el-header>
@@ -35,6 +38,9 @@ export default {
   computed: {
     name() {
       return localStorage.username || '';
+    },
+    type() {
+      return localStorage.userType || '';
     }
   }
 }
@@ -58,15 +64,15 @@ export default {
       .current {
         position: absolute;
         right: 50px;
-        top: -25px;
+        top: -23px;
         font-size: 14px;
         color: #FFF;
         text-decoration: underline;
       }
       .out {
         position: absolute;
-        top: 30px;
-        right: 50px;
+        top: 40px;
+        right: 66px;
         display: block;
         background-color: #fff;
         font-size: 16px;
