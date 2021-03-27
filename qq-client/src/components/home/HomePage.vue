3107fe87dd8f28098b89ead927eda17d74ec04b0
liaolonghui <1340881930@qq.com>
2021-03-27 11:36:42 +0800

    美化专辑推荐

--- qq-client/src/components/home/HomePage.vue
+++ qq-client/src/components/home/HomePage.vue
@@ -5,11 +5,18 @@
       <div class="album-container">
         <ul class="album-items" :style="{'left': albumLeft+'px'}">
           <li v-for="item in albumData" :key="item._id">
-            <img :src="item.img" :alt="item.name">
+            <figure>
+              <img :src="item.img" :alt="item.name">
+            </figure>
             <p>专辑：{{ item.name }}</p>
             <p>唱片公司：{{ item.company }}</p>
+            <i></i>
           </li>
         </ul>
+        <nav class="album-btn">
+          <i @click="goAlbum1" :class="{ active: albumLeft===0 }"></i>
+          <i @click="goAlbum2" :class="{ active: albumLeft===-1100 }"></i>
+        </nav>
       </div>
       <span id="lt" @click="lt">&lt;</span>
       <span id="gt" @click="gt">&gt;</span>
@@ -36,6 +43,12 @@ export default {
       console.log(this.albumData)
     },
     // album按钮事件
+    goAlbum1 () {
+      this.albumLeft = 0
+    },
+    goAlbum2 () {
+      this.albumLeft = -1100
+    },
     lt () {
       this.albumLeft -= 1100
       if (this.albumLeft < -1100) {
@@ -73,6 +86,25 @@ export default {
     overflow: hidden;
     position: relative;
   }
+  .album-container>.album-btn {
+    position: absolute;
+    width: 40px;
+    top: 90%;
+    left: 50%;
+    margin-left: -20px;
+  }
+  .album-btn>i {
+    display: inline-block;
+    background-color: #ccc;
+    margin: 0 5px;
+    cursor: pointer;
+    width: 9px;
+    height: 9px;
+    border-radius: 50%;
+  }
+  .album-btn>i.active {
+    background-color: #aaa;
+  }
   .album-items {
     height: 100%;
     width: 200%;
@@ -82,16 +114,42 @@ export default {
   .album-items>li {
     float: left;
     width: 10%;
-    height: 90%;
+    height: 75%;
     padding: 0 20px;
     font-size: 12px;
+    position: relative;
   }
-  .album-items>li>img {
+  .album-items>li>i {
+    opacity: 0;
+    width: 60px;
+    height: 60px;
+    background: url('../../assets/imgs/cover.png') no-repeat;
+    background-size: contain;
+    position: absolute;
+    top: 65px;
+    left: 80px;
+    transform: scale(0.7);
+    transition-property: opacity transform;
+    transition-duration: 0.5s;
+  }
+  .album-items>li:hover>i {
+    display: block;
+    transform: scale(1);
+    opacity: 1;
+  }
+  .album-items>li>figure {
+    width: 100%;
+    height: 180px;
+    margin: 0 0 10px 0;
+    overflow: hidden;
+  }
+  .album-items>li img {
+    display: block;
     width: 100%;
     cursor: pointer;
-    transition: width opacity 0.7s ease-in;
+    transition: width 0.2s ease-in;
   }
-  .album-items img:hover {
+  .album-items>li:hover img {
     width: 110%;
     opacity: 0.8;
   }
